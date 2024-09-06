08f7a2c50b26b7d71e00bf98b7a912560dddeebb
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2024-09-06 11:10:50 -0400

    Menu incial del carrito de compŕas

--- src/cl/duoc/cmartinez/dsy1121/Main.java
+++ src/cl/duoc/cmartinez/dsy1121/Main.java
@@ -1,12 +1,24 @@
 package cl.duoc.cmartinez.dsy1121;
 
+
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
     Producto producto1 = new Producto("Producto 1", "Nombre 1", 10);
     Producto producto2 = new Producto("Producto 2", "Nombre 2", 20);
     Producto producto3 = new Producto("Producto 3", "Nombre 3", 30);
 
-    Cliente cliente1 = new Cliente("Cliente 1", "Nombre 1");
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Ingrese el nombre del cliente: ");
+    String nombre = scan.nextLine();
+
+    Cliente cliente1 = new Cliente("Cliente 1", nombre);
+    int opcion = 0;
+    do {
+      System.out.print("Selecione una opcion: ");
+      opcion = scan.nextInt();
+    } while (opcion != 5);
 
     cliente1.agregarProducto(producto1);
     cliente1.agregarProducto(producto2);
